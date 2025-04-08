7abe2ccfe30399593446dd65f8d83654a7795baf
nexphernandez <nicolaseh3b1112@gmail.com>
2025-04-08 18:57:51 +0100

    Se crea algunas sentencias sql

--- src/main/java/es/nexphernandez/juego/ahorcado/model/servicios/UsuarioServiceModel.java
+++ src/main/java/es/nexphernandez/juego/ahorcado/model/servicios/UsuarioServiceModel.java
@@ -2,6 +2,7 @@ package es.nexphernandez.juego.ahorcado.model.servicios;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import es.nexphernandez.juego.ahorcado.model.UsuarioEntity;
@@ -54,5 +55,112 @@ public class UsuarioServiceModel extends Conexion {
         return usuarios;
     }
 
+    /**
+     * Funcion que actualiza los datos segun la secuencia introducida
+     * @param sql sentencia a realizar
+     * @param usuario datos para actualizar
+     * @return true/false
+     */
+    public boolean actualizarDatos(String sql, UsuarioEntity usuario){
+        try {
+            PreparedStatement sentencia = conectar().prepareStatement(sql);
+            sentencia.setString(1, usuario.getUser());
+            sentencia.setString(2, usuario.getEmail());
+            sentencia.setString(3, usuario.getPassword());
+            sentencia.setInt(4, usuario.getPuntos());
+            sentencia.setInt(5, usuario.getIdNivel());
+            sentencia.execute();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            cerrar();
+        }
+    }
 
+    /**
+     * Funcion para aniadir un usuario a la app
+     * @param usuario a aniadir
+     * @return true/false
+     */
+    public boolean aniadirUsuario(UsuarioEntity usuario){
+        if (usuario == null) {
+            return false;
+        }
+        String sql = "INSERT INTO usuarios (user, email, password, puntos, idNivel) Values (?,?,?,?,?)";
+        return actualizarDatos(sql, usuario);
+    }
+
+    /**
+     * Metodo para obtener los datos de un usuario por el nombre de usuario
+     * @param user nombre de usuario
+     * @return Usuario buscado
+     */
+    public UsuarioEntity obtenerUsuariosPorUser(String user){
+        String sql = "SELECT * FROM usuarios WHERE user = '" + user + "'";
+        ArrayList<UsuarioEntity> usuarios = leerSentencia(sql);
+        if (usuarios.isEmpty()) {
+            return null;
+        }
+        return usuarios.get(0);
+    }
+
+    /**
+     * Metodo para obtener los datos de un usuario por el email
+     * @param email email del usuario buscado
+     * @return usuario buscado
+     */
+    public UsuarioEntity obtenerUsuariosPorEmail(String email){
+        String sql = "SELECT * FROM usuarios WHERE email = '" + email + "'";
+        ArrayList<UsuarioEntity> usuarios = leerSentencia(sql);
+        if (usuarios.isEmpty()) {
+            return null;
+        }
+        return usuarios.get(0);
+    }
+
+    /**
+     * Funcion que te devuleve todos los usuarios de la base de datos
+     * @return lista de usuarios
+     */
+    public ArrayList<UsuarioEntity> obtenerTodosLosUsuarios(){
+        String sql = "SELECT * FROM usuarios";
+        return leerSentencia(sql);
+    }
+
+    /**
+     * Funcion para obtener una palabra segun el nivel del usuario
+     * @param nombreUsuario nombre del usuarop
+     * @return palabra buscada
+     */
+    public String obtenerPalabra (String nombreUsuario){
+        String palabra = null;
+        UsuarioEntity usuario = obtenerUsuariosPorUser(nombreUsuario);
+        try {
+            if (usuario == null) {
+                throw new SQLException("No se encontro el usuario =" + nombreUsuario);
+            }
+            int idNivel = usuario.getIdNivel();
+            String sql = "SELECT palabra FROM palabras WERE id_nivel = ? ORDER BY RANDOM() LIMIT 1";
+            PreparedStatement sentencia = conectar().prepareStatement(sql);
+            sentencia.setInt(1, idNivel);
+            ResultSet resultado = sentencia.executeQuery();
+            if (resultado.next()) {
+                palabra = resultado.getString("palabra");
+            }
+            if (palabra == null || palabra.isEmpty()) {
+                throw new SQLException("No se encontro ninguna palabra para el nivel: " + idNivel);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cerrar();
+        }
+        return palabra;
+    }
+
+    public void actualizarPuntos(){
+        String 
+    }
 }
